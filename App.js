988687284ad90b9c5dbc550ce4308f2ffc8f9bc9
RizkyKii <kurosakii889@gmail.com>
2022-07-27 11:25:28 +0700

    Added my project

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Rizky!</Text>
+    
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  stretch:{
+    width:'100%',
+    height:100
+  }
+});
